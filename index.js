df18d18ab0dd37c9adfb3b2432ee0edce9cf4b2d
Henriquz <jullye.alves@outlook.com>
2026-04-06 11:33:54 -0300

    bot pronto

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { Client, GatewayIntentBits } = require('discord.js');
 const { joinVoiceChannel, createAudioPlayer, createAudioResource, AudioPlayerStatus } = require('@discordjs/voice');
 
+// cria o bot
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
@@ -8,21 +9,22 @@ const client = new Client({
   ]
 });
 
-// 🎯 Lista dos usuários permitidos
+// 🎯 usuários permitidos
 const allowedUsers = [
   "783822938934607922",
   "968663196615471145",
   "878046149754355732"
 ];
 
+// 🎧 áudio
 const AUDIO_FILE = "./audio.mp3";
 
 client.once('ready', () => {
   console.log(`Bot logado como ${client.user.tag}`);
 });
 
+// 🎤 evento de voz
 client.on('voiceStateUpdate', (oldState, newState) => {
-  // entrou no canal
   if (!oldState.channel && newState.channel) {
     const userId = newState.id;
 
@@ -51,4 +53,10 @@ client.on('voiceStateUpdate', (oldState, newState) => {
   }
 });
 
+// login
 client.login(process.env.TOKEN);
+
+// 🔥 FIX DO RENDER (porta fake)
+require('http')
+  .createServer((req, res) => res.end('Bot rodando'))
+  .listen(process.env.PORT || 3000);
